a196d70721bd4bed833862cd0ea70ed0c2ff4e8c
Muhammad Abi Nurhakim <55277741+abinurhakim@users.noreply.github.com>
2024-03-04 20:45:02 +0700

    Add files via upload

---
+++ SQLFinalTaskProject1.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE DWH;
+
+USE DWH;
+
+CREATE TABLE DimCustomer (
+	CustomerID INT NOT NULL,
+	CustomerName VARCHAR(100) NOT NULL,
+	CityName VARCHAR(100) NOT NULL,
+	StateName VARCHAR(100) NOT NULL,
+	Address Varchar(100) NOT NULL,
+	CityID INT NOT NULL,
+	Age INT NOT NULL,
+	Gender VARCHAR(100) NOT NULL,
+	Email VARCHAR(100) NOT NULL,
+	CONSTRAINT PKCustomerID PRIMARY KEY (CustomerID)
+);
+
+CREATE TABLE DimAccount (
+	AccountID INT NOT NULL,
+	CustomerID INT NOT NULL,
+	AccountType VARCHAR(100) NOT NULL,
+	Balance INT NOT NULL,
+	DateOpened DATE NOT NULL,
+	Status VARCHAR(100) NOT NULL,
+	CONSTRAINT PKAccountID PRIMARY KEY (AccountID),
+	CONSTRAINT FKCustomerID FOREIGN KEY (CustomerID) REFERENCES DimCustomer (CustomerID)
+);
+
+CREATE TABLE DimBranch (
+	BranchID INT NOT NULL,
+	BranchName VARCHAR(100) NOT NULL,
+	BranchLocation VARCHAR(100) NOT NULL,
+	CONSTRAINT PKBranchID PRIMARY KEY (BranchID)
+);
+
+DROP TABLE FactTransaction
+
+CREATE TABLE FactTransaction (
+	TransactionID INT NOT NULL,
+	AccountID INT NOT NULL,
+	TransactionDate DATE NOT NULL,
+	Amount INT NOT NULL,
+	TransactionType VARCHAR(100) NOT NULL,
+	BranchID INT NOT NULL,
+	CONSTRAINT PKTransactionID PRIMARY KEY (TransactionID),
+	CONSTRAINT FKAccountID FOREIGN KEY (AccountID) REFERENCES DimAccount (AccountID),
+	CONSTRAINT FKBranchID FOREIGN KEY (BranchID) REFERENCES DimBranch (BranchID)
+);
+
+SELECT * FROM DimCustomer
+SELECT * FROM DimAccount
+SELECT * FROM DimBranch
+SELECT * FROM FactTransaction
---
+++ SQLFinalTaskProjectStoreProcedure1.sql
@@ -0,0 +1,47 @@
+CREATE PROCEDURE DailyTransactions
+    @start_date DATE,
+    @end_date DATE
+AS
+BEGIN
+    SELECT
+		TransactionDate AS [Date],
+        COUNT(TransactionID) AS TotalTransactions,
+        SUM(Amount) AS TotalAmount
+    FROM
+        FactTransaction 
+    WHERE
+        TransactionDate BETWEEN @start_date AND @end_date
+	GROUP BY
+		TransactionDate
+END;
+
+EXEC DailyTransactions @start_date ='2024-01-18', @end_date= '2024-01-20'
+
+
+
+CREATE PROCEDURE BalancePerCustomers
+    @name NVARCHAR(50)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        c.CustomerName,
+        a.AccountType,
+        a.Balance,
+        a.Balance - ISNULL(SUM(CASE WHEN t.TransactionType = 'Deposit' THEN t.Amount ELSE -t.Amount END), 0) AS CurrentBalance
+    FROM 
+        DimCustomer c
+    INNER JOIN 
+        DimAccount a ON c.CustomerID = a.CustomerID
+    LEFT JOIN 
+        FactTransaction t ON a.AccountID = t.AccountID
+    WHERE 
+        c.CustomerName = @name
+        AND a.Status = 'active'
+    GROUP BY 
+        c.CustomerName, a.AccountType, a.Balance;
+END
+
+EXEC BalancePerCustomers @name = 'Shelly'
+EXEC BalancePerCustomers @name = 'SHELLY JUWITA'
